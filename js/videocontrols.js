2bd66103284b8d3102b6529bd80bf27587a2cc9f
Tinnahalls <tinnahalls@gmail.com>
2020-11-24 08:39:04 +0000

    Working on Video Controls

--- js/videocontrols.js
+++ js/videocontrols.js
@@ -1,6 +1,5 @@
 var supportsVideo = !!document.createElement('video').canPlayType;
 if (supportsVideo) {
-}
 
 var videoContainer = document.getElementById('videoContainer');
 var video = document.getElementById('video');
@@ -20,3 +19,53 @@ var voldec = document.getElementById('voldec');
 var progress = document.getElementById('progress');
 var progressBar = document.getElementById('progress-bar');
 var fullscreen = document.getElementById('fs');
+
+// Set up Player Controls
+
+playpause.addEventListener('click', function(e) {
+  if (video.paused || video.ended) video.play();
+  else video.pause();
+});
+
+stop.addEventListener('click', function(e) {
+  video.pause();
+  video.currentTime = 0;
+  progress.value = 0;
+});
+
+mute.addEventListener('click', function(e) {
+  video.muted = !video.muted;
+});
+
+// Buttons for Volume
+volinc.addEventListener('click', function(e) {
+  alterVolume('+');
+});
+voldec.addEventListener('click', function(e) {
+  alterVolume('-');
+});
+
+
+// Volume
+var alterVolume = function(dir) {
+  var currentVolume = Math.floor(video.volume * 10) / 10;
+  if (dir === '+') {
+     if (currentVolume < 1) video.volume += 0.1;
+  }
+  else if (dir === '-') {
+     if (currentVolume > 0) video.volume -= 0.1;
+  }
+}
+
+// Progress bar for Video
+video.addEventListener('loadedmetadata', function() {
+  progress.setAttribute('max', video.duration);
+});
+
+// Updates the Progress bar
+video.addEventListener('timeupdate', function() {
+  progress.value = video.currentTime;
+  progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+});
+
+}
